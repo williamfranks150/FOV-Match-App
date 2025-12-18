085cbdf6f1637f1cdd4d48210ed7821caa1e91f4
williamfranks150 <william.franks@gmail.com>
2025-12-18 09:52:12 -0500

    Simplify caching logic in service worker
    Refactor service worker to simplify caching logic and remove unused event listeners.

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
 const CACHE = "fov-match-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    caches.open(CACHE).then((c) => c.add("./"))
   );
-  self.clients.claim();
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
+    fetch(e.request).then((r) => {
+      const copy = r.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request).then((r) => r || caches.match("./")))
   );
 });
